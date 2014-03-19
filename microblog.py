75b2dbc66cd890c629c70b9fcd4017fae436fdf7
Luke Petschauer <lpetschauer@gmail.com>
2014-03-18 19:41:01 -0700

    working to improve

--- microblog.py
+++ microblog.py
@@ -41,6 +41,11 @@ class Post(db.Model):
         return '<Post %r>' % self.title
 
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20), unique=True)
+
+
 @app.route('/')
 def index():
     return render_template('layout.html')

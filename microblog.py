65e45a1a8aac519b1714db10d41411bff26dcdab
Luke Petschauer <lpetschauer@gmail.com>
2014-03-20 00:30:12 -0700

    update before rollback

--- microblog.py
+++ microblog.py
@@ -43,8 +43,24 @@ class Post(db.Model):
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(20), unique=True)
+    username = db.Column(db.String(20), unique=True)
+    password = []
+    email = []
+    posts = []
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
 
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20), unique=True)
+    users = db.relationship('User', backref='role')
+
+    def __repr__(self):
+        return '<User %r>' % self.name
 
 @app.route('/')
 def index():

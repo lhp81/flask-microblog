72fa31f893ce2485a66b1a3365a1cd07663b3626
Luke Petschauer <lpetschauer@gmail.com>
2014-03-20 00:30:30 -0700

    Revert 67ec158..65e45a1
    This rolls back to commit 67ec158995d4982eac2bffa88848391432dea997.

--- microblog.py
+++ microblog.py
@@ -1,23 +1,15 @@
 from flask import Flask
-from flask import request
 from flask.ext.sqlalchemy import SQLAlchemy
-# from flaskext.seasurf import SeaSurf
 from datetime import datetime
 from flask.ext.script import Manager
 from flask.ext.migrate import Migrate, MigrateCommand
-from flaskext.bcrypt import Bcrypt
-from flask import render_template
-from flask.ext.bootstrap import Bootstrap
 
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///flaskblog'
 
 db = SQLAlchemy(app)
-# csrf = SeaSurf(app)
 migrate = Migrate(app, db)
-bcrypt = Bcrypt(app)
-bootstrap = Bootstrap(app)
 
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
@@ -28,7 +20,6 @@ class Post(db.Model):
     title = db.Column(db.String(100), unique=True)
     body = db.Column(db.Text)
     pub_date = db.Column(db.DateTime)
-    auth_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
     def __init__(self, title, body, pub_date=None):
         self.title = title
@@ -41,55 +32,10 @@ class Post(db.Model):
         return '<Post %r>' % self.title
 
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), unique=True)
-    password = []
-    email = []
-    posts = []
-    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
-
-    def __repr__(self):
-        return '<User %r>' % self.username
-
-
-class Role(db.Model):
-    __tablename__ = 'roles'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(20), unique=True)
-    users = db.relationship('User', backref='role')
-
-    def __repr__(self):
-        return '<User %r>' % self.name
-
-@app.route('/')
-def index():
-    return render_template('layout.html')
-
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    return render_template('login.html')
-    # if request.method == 'POST':
-    #     login()
-    # else:
-    #     show_login_form()
-
-
-# @app.route('/post', methods=['GET', 'POST'])
-# @app.route('/logout')
-# def log_out():
-#     if request.method == 'POST':
-#         log_out()
-#     else:
-#         show_login_form()
-
-@app.route('/new')
 def write_post(title, text):
     new_post = Post(title, text)
     db.session.add(new_post)
     db.session.commit()
-    return render_template('post.html')
 
 
 def read_all_posts():
@@ -106,15 +52,5 @@ def read_a_post(id):
     else:
         return the_post
 
-
-@app.route('/login')
-def login(id, password):
-    pass
-
-
-
-
-
-
 if __name__ == '__main__':
-    manager.run()
+    manager.run(debug=True)
--- readme.md
+++ readme.md
@@ -3,5 +3,3 @@ This Flask-powered microblog has three methods:
 * **write_post(title, text)**: will create a new post based on the title and text supplied.
 * **read_posts()**: will return a list of the post objects in the database, with the most recently created posts first.
 * **read_post(id)**: will return the post identified by id or raise an appropriate error if none is found.
-
-It also supports user registration, post categories, and is prettified via bootstrap.

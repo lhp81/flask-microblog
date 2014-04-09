cadf8b8817901e2710a9eab6854c3a10ad06b000
Luke Petschauer <lpetschauer@gmail.com>
2014-04-08 20:42:26 -0700

    back in the saddle
    Let users post original poet name.
    Update templates.
    Start to work on password encryption and registration.

--- microblog.py
+++ microblog.py
@@ -7,9 +7,13 @@ from flask.ext.script import Manager
 from flask.ext.migrate import Migrate, MigrateCommand
 from flask.ext.bootstrap import Bootstrap
 from flask_mail import Mail, Message
+from passlib.apps import custom_app_context as pwd_context
+from flaskext.bcrypt import Bcrypt
 
 
 app = Flask(__name__)
+
+bcrypt = Bcrypt(app)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///flaskblog'
 # app.config['MAIL_SERVER'] = 'smtp.gmail.com'
@@ -40,6 +44,7 @@ class Post(db.Model):
     title = db.Column(db.String(80), unique=True)
     body = db.Column(db.Text)
     author_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    poet = db.Column(db.String(80))
     pub_date = db.Column(db.DateTime)
 
     def __init__(self, title, body, author, pub_date=None):
@@ -71,7 +76,7 @@ class User(db.Model):
     # __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True)
-    password = db.Column(db.String(40))
+    password_hash = db.Column(db.String(128))
     email = db.Column(db.String, unique=True)
     user_posts = db.relationship("Post", backref="author", lazy='dynamic')
     # confirmed = db.Column(db.Boolean)
@@ -86,7 +91,7 @@ class User(db.Model):
         return '<User %r>' % self.username
 
 
-def write_post(title, text, author):
+def write_post(title, text, poet, author):
     if title and text:
         new_post = Post(title, text, author)
         db.session.add(new_post)
@@ -99,15 +104,6 @@ def write_post(title, text, author):
 def read_posts():
     """Display posts in reverse order."""
     return Post.query.order_by(Post.id.desc()).all()
-
-# def read_post(id):
-#     the_post = Post.query.filter_by(id=id).first()
-#     if not Post:
-#         raise IndexError('Someone there is who cannot find a post.\n'
-#                          'Oh, it\'s you!\n'
-#                          'That post doesn\'t exist!')
-#     else:
-#         return the_post
 
 
 def add_user(username, email, password):
@@ -135,6 +131,7 @@ def add_poem():
         if request.method == "POST":
             write_post(request.form['title'],
                        request.form['body'],
+                       request.form['poet'],
                        session['current_user'])
             return redirect(url_for('all_posts'))
     return render_template('compose.html')
@@ -186,7 +183,7 @@ def logout():
 @app.route('/<id>')
 def single_poem(id):
     poem = read_poem(id)
-    return render_template('poem.html', post=poem)
+    return render_template('poem.html', post=poem, orig_poet=poet)
 
 def read_poem(id):
     """Retrieve a single post by its id."""
